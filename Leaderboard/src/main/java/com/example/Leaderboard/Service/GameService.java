22267c45ba4f4a63609cb27f906bc6d73d90fa1a
ayou6 <ayouxb.abbas@gmail.com>
2023-06-19 17:27:43 +0400

    changed the classes to interface

--- Leaderboard/src/main/java/com/example/Leaderboard/Service/GameService.java
+++ Leaderboard/src/main/java/com/example/Leaderboard/Service/GameService.java
@@ -1,4 +1,11 @@
 package com.example.Leaderboard.Service;
 
+import com.example.Leaderboard.Repository.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class GameService {
+    @Autowired
+    GameRepository gameRepository;
 }
