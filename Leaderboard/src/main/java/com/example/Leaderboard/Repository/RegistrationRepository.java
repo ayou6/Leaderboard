62e780a3ae3ec81147e77d573aeba197a57a6ee6
ayoxb <ayoxb.abbas@gmail.com>
2023-06-19 23:46:29 +0400

    editting the Registration function

--- Leaderboard/src/main/java/com/example/Leaderboard/Repository/RegistrationRepository.java
+++ Leaderboard/src/main/java/com/example/Leaderboard/Repository/RegistrationRepository.java
@@ -1,6 +1,10 @@
 package com.example.Leaderboard.Repository;
 
+import com.example.Leaderboard.Model.Registration;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface RegistrationRepository {
+@Repository
+public interface RegistrationRepository extends JpaRepository<Registration, Long> {
 
 }
