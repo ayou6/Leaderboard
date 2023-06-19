4a564f840b04c8f967f54a2c538d0efdbb5aa545
ayou6 <ayouxb.abbas@gmail.com>
2023-06-19 17:27:51 +0400

    changed the classes to interface

--- Leaderboard/src/main/java/com/example/Leaderboard/Repository/GameRepository.java
+++ Leaderboard/src/main/java/com/example/Leaderboard/Repository/GameRepository.java
@@ -1,4 +1,10 @@
 package com.example.Leaderboard.Repository;
 
-public class GameRepository {
+import com.example.Leaderboard.Model.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GameRepository extends JpaRepository<Game,Long> {
+
 }
