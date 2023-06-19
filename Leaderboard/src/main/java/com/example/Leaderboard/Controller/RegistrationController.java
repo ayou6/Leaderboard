e1f6e4d5133b82c6c79eedd0cda62490d8978ec9
ayoxb <ayoxb.abbas@gmail.com>
2023-06-19 23:46:19 +0400

    editting the Registration function

--- Leaderboard/src/main/java/com/example/Leaderboard/Controller/RegistrationController.java
+++ Leaderboard/src/main/java/com/example/Leaderboard/Controller/RegistrationController.java
@@ -1,15 +1,28 @@
 package com.example.Leaderboard.Controller;
 
+import com.example.Leaderboard.Model.Registration;
+import com.example.Leaderboard.Service.RegistrationService;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-
+@RestController
+@RequestMapping("api/register")
 public class RegistrationController {
+    @Autowired
+    RegistrationService registrationService;
+    @PostMapping
+    public Registration createTeam(Registration registration) {
+        return registrationService.createTeam(registration);}
+
 
 }
