e8d6f203c35c33f9ea2d5548994d1472adb9572a
ayou6 <ayouxb.abbas@gmail.com>
2023-06-19 12:19:04 +0400

    Attirbutes adding to class Team

--- Leaderboard/src/main/java/com/example/Leaderboard/Model/Team.java
+++ Leaderboard/src/main/java/com/example/Leaderboard/Model/Team.java
@@ -1,4 +1,12 @@
 package com.example.Leaderboard.Model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
 }
