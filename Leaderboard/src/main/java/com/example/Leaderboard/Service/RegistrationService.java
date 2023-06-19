c606ff2193aa3ff3a4b0160efa2e26abd386110b
ayoxb <ayoxb.abbas@gmail.com>
2023-06-19 23:46:38 +0400

    editting the Registration function

--- Leaderboard/src/main/java/com/example/Leaderboard/Service/RegistrationService.java
+++ Leaderboard/src/main/java/com/example/Leaderboard/Service/RegistrationService.java
@@ -1,6 +1,15 @@
 package com.example.Leaderboard.Service;
 
+import com.example.Leaderboard.Model.Registration;
+import com.example.Leaderboard.Repository.RegistrationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class RegistrationService {
-
+@Autowired
+    RegistrationRepository registrationRepository;
+    public Registration createTeam(Registration registration) {
+        return registrationRepository.save(registration);
+    }
 }
