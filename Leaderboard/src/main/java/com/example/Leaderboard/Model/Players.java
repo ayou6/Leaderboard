cd2a791b79118895969a3d4115e10686ff75ba05
ayou6 <ayouxb.abbas@gmail.com>
2023-06-19 12:12:03 +0400

    adding attirbutes

--- Leaderboard/src/main/java/com/example/Leaderboard/Model/Players.java
+++ Leaderboard/src/main/java/com/example/Leaderboard/Model/Players.java
@@ -1,4 +1,19 @@
 package com.example.Leaderboard.Model;
 
+import lombok.*;
+
+
+import javax.persistence.*;
+@Data
+
+
 public class Players {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+
+
+
+
 }
