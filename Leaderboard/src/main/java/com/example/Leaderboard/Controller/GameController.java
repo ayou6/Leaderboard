56c999b85883d40e8609b93d37a0d64b30c19a4d
ayou6 <ayouxb.abbas@gmail.com>
2023-06-19 18:11:00 +0400

    added getAllGames

--- Leaderboard/src/main/java/com/example/Leaderboard/Controller/GameController.java
+++ Leaderboard/src/main/java/com/example/Leaderboard/Controller/GameController.java
@@ -1,4 +1,25 @@
 package com.example.Leaderboard.Controller;
 
+import com.example.Leaderboard.Model.Game;
+import com.example.Leaderboard.Service.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/games")
 public class GameController {
+        @Autowired
+        GameService gameService;
+
+        @GetMapping("/getAllGames")
+        public List<Game> getAllGames(){
+            return gameService.getAllGames();
+        }
+
+
 }
--- Leaderboard/src/main/java/com/example/Leaderboard/Service/GameService.java
+++ Leaderboard/src/main/java/com/example/Leaderboard/Service/GameService.java
@@ -1,11 +1,17 @@
 package com.example.Leaderboard.Service;
 
+import com.example.Leaderboard.Model.Game;
 import com.example.Leaderboard.Repository.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class GameService {
     @Autowired
     GameRepository gameRepository;
+    public List<Game> getAllGames() {
+        return gameRepository.findAll();
+    }
 }
