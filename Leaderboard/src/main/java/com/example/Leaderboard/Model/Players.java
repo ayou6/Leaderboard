822777db8543a2e9092c58312206e91350be7204
ayou6 <ayouxb.abbas@gmail.com>
2023-06-19 12:12:29 +0400

    adding attirbutes

--- Leaderboard/src/main/java/com/example/Leaderboard/Model/Players.java
+++ Leaderboard/src/main/java/com/example/Leaderboard/Model/Players.java
@@ -1,11 +1,14 @@
 package com.example.Leaderboard.Model;
 
-import lombok.*;
-
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 @Data
-
+@Getter
+@Setter
+@Entity
 
 public class Players {
     @Id
