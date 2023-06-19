a71935d36759bef5098b7d34704463e037f73e88
ayou6 <ayouxb.abbas@gmail.com>
2023-06-19 14:59:43 +0400

    adding relationships

--- Leaderboard/src/main/java/com/example/Leaderboard/Model/Game.java
+++ Leaderboard/src/main/java/com/example/Leaderboard/Model/Game.java
@@ -3,12 +3,15 @@ package com.example.Leaderboard.Model;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 public class Game {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long id;
+    @OneToMany
     public Team team1;
+    @OneToMany
     public Team team2;
     public int score1;
     public int score2;
