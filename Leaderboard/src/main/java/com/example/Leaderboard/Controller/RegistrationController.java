ca9fcbad93956db10ee497d6c57c3503dac1847d
ayou6 <ayouxb.abbas@gmail.com>
2023-06-20 11:22:40 +0400

    add request body because  when the registration was saved in the database it was null and request-body solved the issue occurred

--- Leaderboard/src/main/java/com/example/Leaderboard/Controller/RegistrationController.java
+++ Leaderboard/src/main/java/com/example/Leaderboard/Controller/RegistrationController.java
@@ -1,19 +1,11 @@
 package com.example.Leaderboard.Controller;
-
 import com.example.Leaderboard.Model.Registration;
 import com.example.Leaderboard.Service.RegistrationService;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+
+
 
 @RestController
 @RequestMapping("api/register")
@@ -21,8 +13,9 @@ public class RegistrationController {
     @Autowired
     RegistrationService registrationService;
     @PostMapping
-    public Registration createTeam(Registration registration) {
-        return registrationService.createTeam(registration);}
+    public Registration createTeam(@RequestBody Registration registration) {
+        return registrationService.createTeam(registration);
+    }
 
 
 }
