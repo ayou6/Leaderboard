62bf547f72afb18150258f01e2c09ebebc3bb0fa
ayou6 <ayouxb.abbas@gmail.com>
2023-06-20 16:08:51 +0400

    creating assign game function

--- Leaderboard/src/main/java/com/example/Leaderboard/Repository/TeamRepository.java
+++ Leaderboard/src/main/java/com/example/Leaderboard/Repository/TeamRepository.java
@@ -1,4 +1,9 @@
 package com.example.Leaderboard.Repository;
 
-public interface TeamRepository {
+import com.example.Leaderboard.Model.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TeamRepository extends JpaRepository<Team, Long> {
 }
