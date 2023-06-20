83e5dc3366c0f2c8af3c205ea737274bee715972
ayou6 <ayouxb.abbas@gmail.com>
2023-06-20 16:08:45 +0400

    creating assign game function

--- Leaderboard/src/main/java/com/example/Leaderboard/Controller/TeamController.java
+++ Leaderboard/src/main/java/com/example/Leaderboard/Controller/TeamController.java
@@ -1,4 +1,20 @@
 package com.example.Leaderboard.Controller;
 
+import com.example.Leaderboard.Model.Team;
+import com.example.Leaderboard.Service.TeamService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/register")
 public class TeamController {
+    @Autowired
+    TeamService teamService;
+    @PostMapping
+    public Team createTeam(@RequestBody Team team) {
+        return teamService.createTeam(team);
+    }
 }
