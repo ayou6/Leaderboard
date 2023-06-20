c030a3af22d2ce41a355eef7129350a0d59b2dcf
ayou6 <ayouxb.abbas@gmail.com>
2023-06-20 16:08:57 +0400

    creating assign game function

--- Leaderboard/src/main/java/com/example/Leaderboard/Service/TeamService.java
+++ Leaderboard/src/main/java/com/example/Leaderboard/Service/TeamService.java
@@ -1,4 +1,17 @@
 package com.example.Leaderboard.Service;
 
+import com.example.Leaderboard.Model.Team;
+import com.example.Leaderboard.Repository.TeamRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TeamService {
+    @Autowired
+    TeamRepository teamRepository;
+
+    public Team createTeam(Team team) {
+        return teamRepository.save(team);
+    }
+
 }
